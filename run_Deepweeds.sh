4aab6b04bbe36cff1876c3160c588a95aae0eb4e
Etienne Lord <etienne_lord@hotmail.com>
2025-07-14 10:59:12 -0400

    Update run_Deepweeds.sh

--- run_Deepweeds.sh
+++ run_Deepweeds.sh
@@ -20,14 +20,14 @@ do
         echo "i=$i /10 k=$ik seed=$iseed"
         rm -rf DeepWeeds_*
         python setup_k_dataset.py --data DeepWeeds --output DeepWeeds_$ik --seed=$iseed --k=$ik --complete --max_test=250
-        CUDA_VISIBLE_DEVICES=0 python generic_mobilenet3.py --epoch=50 --data_dir DeepWeeds_$ik --output output_PlantSeedling_generic_mobilenet_$ik --batch_size=128 --imgsz=256 --seed=$iseed
-        CUDA_VISIBLE_DEVICES=0 python generic_resnet50.py --epoch=50 --data_dir DeepWeeds_$ik --output output_PlantSeedling_generic_resnet50_$ik --batch_size=128 --imgsz=256 --seed=$iseed
-        CUDA_VISIBLE_DEVICES=0 python train.py --epoch=50 --data DeepWeeds_$ik --tree DeepWeeds/taxonomy.txt --output output_DeepWeeds_mobilenet_taxonomic_$ik --batch_size=128 --imgsz=256 --lr=0.001 --model "mobilenet" --seed=$iseed --embeddings_size=64 --loss "taxonomic" 
-        CUDA_VISIBLE_DEVICES=0 python train.py --epoch=50 --data DeepWeeds_$ik --tree DeepWeeds/taxonomy.txt --output output_DeepWeeds_resnet_taxonomic_$ik --batch_size=128 --imgsz=256 --lr=0.001 --model "resnet50" --seed=$iseed --embeddings_size=64 --loss "taxonomic" 
-        CUDA_VISIBLE_DEVICES=0 python train.py --epoch=50 --data DeepWeeds_$ik --tree DeepWeeds/taxonomy.txt --output output_DeepWeeds_mobilenet_triplet_$ik --batch_size=128 --imgsz=256 --lr=0.001 --model "mobilenet" --seed=$iseed --embeddings_size=64 --loss "triplet" 
-        CUDA_VISIBLE_DEVICES=0 python train.py --epoch=50 --data DeepWeeds_$ik --tree DeepWeeds/taxonomy.txt --output output_DeepWeeds_resnet_triplet_$ik --batch_size=128 --imgsz=256 --lr=0.001 --model "resnet50" --seed=$iseed --embeddings_size=64 --loss "triplet" 
-        CUDA_VISIBLE_DEVICES=0 python train.py --epoch=50 --data DeepWeeds_$ik --tree DeepWeeds/taxonomy.txt --output output_DeepWeeds_mobilenet_htl_$ik --batch_size=128 --imgsz=256 --lr=0.001 --model "mobilenet" --seed=$iseed --embeddings_size=64 --loss "htl" 
-        CUDA_VISIBLE_DEVICES=0 python train.py --epoch=50 --data DeepWeeds_$ik --tree DeepWeeds/taxonomy.txt --output output_DeepWeeds_resnet_htl_$ik --batch_size=128 --imgsz=256  --lr=0.001 --model "resnet50" --seed=$iseed --embeddings_size=64 --loss "htl" 
+        CUDA_VISIBLE_DEVICES=0 python generic_mobilenet3.py --epoch=50 --data_dir DeepWeeds_$ik --output output_PlantSeedling_generic_mobilenet_$ik --batch_size=128 --imgsz=224 --seed=$iseed
+        CUDA_VISIBLE_DEVICES=0 python generic_resnet50.py --epoch=50 --data_dir DeepWeeds_$ik --output output_PlantSeedling_generic_resnet50_$ik --batch_size=128 --imgsz=224 --seed=$iseed
+        CUDA_VISIBLE_DEVICES=0 python train.py --epoch=50 --data DeepWeeds_$ik --tree DeepWeeds/taxonomy.txt --output output_DeepWeeds_mobilenet_taxonomic_$ik --batch_size=128 --imgsz=224 --lr=0.001 --model "mobilenet" --seed=$iseed --embeddings_size=64 --loss "taxonomic" 
+        CUDA_VISIBLE_DEVICES=0 python train.py --epoch=50 --data DeepWeeds_$ik --tree DeepWeeds/taxonomy.txt --output output_DeepWeeds_resnet_taxonomic_$ik --batch_size=128 --imgsz=224 --lr=0.001 --model "resnet50" --seed=$iseed --embeddings_size=64 --loss "taxonomic" 
+        CUDA_VISIBLE_DEVICES=0 python train.py --epoch=50 --data DeepWeeds_$ik --tree DeepWeeds/taxonomy.txt --output output_DeepWeeds_mobilenet_triplet_$ik --batch_size=128 --imgsz=224 --lr=0.001 --model "mobilenet" --seed=$iseed --embeddings_size=64 --loss "triplet" 
+        CUDA_VISIBLE_DEVICES=0 python train.py --epoch=50 --data DeepWeeds_$ik --tree DeepWeeds/taxonomy.txt --output output_DeepWeeds_resnet_triplet_$ik --batch_size=128 --imgsz=224 --lr=0.001 --model "resnet50" --seed=$iseed --embeddings_size=64 --loss "triplet" 
+        CUDA_VISIBLE_DEVICES=0 python train.py --epoch=50 --data DeepWeeds_$ik --tree DeepWeeds/taxonomy.txt --output output_DeepWeeds_mobilenet_htl_$ik --batch_size=128 --imgsz=224 --lr=0.001 --model "mobilenet" --seed=$iseed --embeddings_size=64 --loss "htl" 
+        CUDA_VISIBLE_DEVICES=0 python train.py --epoch=50 --data DeepWeeds_$ik --tree DeepWeeds/taxonomy.txt --output output_DeepWeeds_resnet_htl_$ik --batch_size=128 --imgsz=224  --lr=0.001 --model "resnet50" --seed=$iseed --embeddings_size=64 --loss "htl" 
     done
 done
 
